fa885069e49789c2f9a92b76589a1162a7ec9b42
david.tian <david.tian@live.com>
2020-03-08 19:55:19 +0800

    test

--- django/orm/myorm/myorm/asgi.py
+++ django/orm/myorm/myorm/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for orm project.
+ASGI config for myorm project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myorm.settings')
 
 application = get_asgi_application()
